d7b2f92f98c802f199695d7dbab29502d28b5cac
Michael Wang <hi@michael.wang>
2020-03-30 17:03:26 -0700

    Initial commit

---
+++ src/plugin/code.ts
@@ -0,0 +1,48 @@
+switch(figma.command) {
+    case 'setKey':
+      break
+    case 'copyText':
+      if(figma.currentPage.selection.length > 1) {
+        figma.notify("Please select just one item")
+      } else {
+        copyText(figma.currentPage.selection[0])
+      }
+      break
+  }
+  
+  figma.clientStorage.getAsync('apikey').then( response => {
+    console.log(response)
+  });
+  
+  async function setKey() {
+    figma.showUI(__html__)
+    figma.ui.postMessage({
+      command: figma.command,
+      key: await figma.clientStorage.getAsync('apikey')
+    })
+  
+    figma.ui.onmessage = async (msg) => {
+      console.log(msg)
+      figma.closePlugin()
+    }
+  }
+  
+  async function copyText(node) {
+    for (const paint of node.fills) {
+      if (paint.type === 'IMAGE') {
+        const image = figma.getImageByHash(paint.imageHash)
+        const bytes = await image.getBytesAsync()
+  
+        figma.showUI(__html__)
+        figma.ui.postMessage({
+          command: figma.command,
+          bytes: bytes,
+          key: await figma.clientStorage.getAsync('apikey')
+        })
+  
+        figma.ui.onmessage = async (msg) => {
+          figma.closePlugin();
+        }
+      }
+    }
+  }
